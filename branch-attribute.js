ed0e49dac50452d316e54305988dd8ce79ad6ef8
Vinnie Falco <4302819+unecigarettewm@users.noreply.github.com>
2023-03-11 01:37:54 -0800

    add site docs and refactor lib docs

---
+++ branch-attribute.js
@@ -0,0 +1,35 @@
+'use strict'
+
+class Ext {
+    static register() {
+        new Ext(this)
+    }
+
+    constructor(ctx) {
+        ;(this.ctx = ctx)
+        .on('contextStarted', this.onContextStarted.bind(this))
+    }
+
+    onContextStarted({playbook}) {
+        var version = playbook.asciidoc.attributes.branch
+        if(version) {
+            if( version == "develop" ||
+                version == "master" ||
+                version == "HEAD") {
+                playbook.content.branches = version
+                console.log( "branches: ", playbook.content.branches)
+            } else {
+                playbook.content.branches = "~" // disable branches
+                playbook.content.tags = "boost-" + version
+                console.log( "tags: ", playbook.content.tags)
+            }
+        } else {
+            playbook.content.branches = "HEAD"
+            console.log( "branches: ", playbook.content.branches)
+        }
+        if(playbook.output.dir == "./build") // VFALCO MASSIVE HACK
+            playbook.output.dir += "/" + version + "/libs"
+    }
+}
+
+module.exports = Ext
